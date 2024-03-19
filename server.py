028bfc0cd050a817f48a797541c488d2985de6e9
JP Flouret <jpflouret@gmail.com>
2024-03-19 13:56:15 -0700

    Add /healthz endpoint

--- server.py
+++ server.py
@@ -11,10 +11,22 @@ class MyServer(BaseHTTPRequestHandler):
   def do_GET(self):
     self.server_version = 'Teapot/1.0.0'
     self.sys_version = 'Coffee/1.0.0'
+    if self.path == '/healthz':
+      self.healthz()
+    else:
+        self.teapot()
+
+  def teapot(self):
     self.send_response(418)
     self.send_header("Content-type", "text/plain; charset=utf-8")
     self.end_headers()
     self.wfile.write(bytes("I'm a teapot.\n", "utf-8"))
+
+  def healthz(self):
+    self.send_response(200)
+    self.send_header("Content-type", "text/plain; charset=utf-8")
+    self.end_headers()
+    self.wfile.write(bytes("OK\n", "utf-8"))
 
 if __name__ == "__main__":
   webServer = HTTPServer((bindAddress, serverPort), MyServer)
